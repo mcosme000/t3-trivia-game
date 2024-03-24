354efae987157e39e0c2e2e2f072ac029a9acd15
Maria Cosmen <mcosme00@gmail.com>
2024-03-24 19:32:52 +0900

    feat/add default image when user is not logged in

--- src/components/Organisms/navbar/index.tsx
+++ src/components/Organisms/navbar/index.tsx
@@ -1,9 +1,19 @@
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import AtomsAvatar from "@/components/atoms/avatar";
 
 const OrganismsNavbar = () => {
+  const { data: session } = useSession();
+
   return (
-    <nav>
-      <p>Navbar test</p>
+    <nav className="absolute right-5 flex px-4 py-6 items-center gap-4">
+      <p>My scores</p>
+      <div className="w-[30px] h-[30px] rounded-full">
+        <AtomsAvatar
+          src={session?.user.image || "/images.png"}
+          alt="user's avatar"
+          className="h-full w-full"
+        />
+      </div>
     </nav>
   );
 };
--- src/components/atoms/avatar/index.tsx
+++ src/components/atoms/avatar/index.tsx
@@ -1,14 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface AtomAvatarProps {
+  className?: string;
   src: string | undefined;
   alt?: string;
 }
 
-const AtomsAvatar: React.FC<AtomAvatarProps> = ({ src, alt }) => {
+const AtomsAvatar = ({ className, src, alt }: AtomAvatarProps) => {
   return (
     <Avatar>
-      <AvatarImage src={src} alt={alt} />
+      <AvatarImage className={className} src={src} alt={alt} />
     </Avatar>
   );
 };
